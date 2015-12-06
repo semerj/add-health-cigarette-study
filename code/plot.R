bf37f5408bd31c43ca4ac3d537c640f963fe37ff
semerdj <jsemer@gmail.com>
2015-12-06 01:17:39 -0800

    new plots

--- code/plot.R
+++ code/plot.R
@@ -1,32 +1,66 @@
 library(ggplot2)
+library(tidyr)
+library(dplyr)
 
-add = read.csv('../data/add_recode.csv',
-               colClasses = c('factor', 'integer', 'factor', 'factor', 'factor',
-                              'factor', 'factor', 'factor', 'factor', 'numeric'))
-
-# add %>%
-#   group_by(cigarettes, wave) %>%
-#   summarize(mu = mean(CESD))
-
+load('../data/add_cig.RData')
+load('../data/add_waves.RData')
 
 ################################################################################
 ###################################  Plots  ####################################
 ################################################################################
 
-ggplot(add, aes(y=CESD, x=age, col=sex)) +
-  geom_smooth(method='loess')
+# Proportion of smokers by wave & sex
+add_waves %>%
+  group_by(wave, sex, cigarettes) %>%
+  tally() %>%
+  group_by(wave, sex) %>%
+  mutate(pct=n/sum(n)) %>%
+  filter(cigarettes == 1) %>%
+  ggplot(aes(x = wave, y = pct, col = sex, group=sex)) +
+  geom_line()
 
-# cigarettes
-ggplot(add, aes(x = wave, y = CESD, color = cigarettes)) +
-  geom_boxplot(position="dodge") +
-  facet_wrap(~sex)
+# Each sex should have it's own intercept
+add_waves %>%
+  head(200) %>%
+  ggplot(aes(x = wave, y = CESD, group = AID, col = as.factor(AID))) +
+  geom_line(position=position_jitter(w = 0, h = 0.2)) +
+  theme_bw() +
+  theme(legend.position="none") +
+  facet_wrap(~sex) +
+  ggtitle('Random sample of 50 subjects')
 
-# marijuana
-ggplot(add, aes(x = wave, y = CESD, color = marijuana)) +
-  geom_boxplot(position="dodge") +
-  facet_wrap(~sex)
+# Depression went up during the second wave of the study for individuals that
+# smoked during the baseline period
+add_cig$cigarettes = as.factor(add_cig$cigarettes)
+ggplot(add_cig, aes(x = wave, y = CESD_diff, fill=cigarettes)) +
+  geom_boxplot() +
+  theme_bw()
 
-# drink
-ggplot(add, aes(x = wave, y = CESD, color = drink)) +
-  geom_boxplot(position="dodge") +
-  facet_wrap(~sex)
+# Save as above
+mu = add_cig %>% group_by(wave, cigarettes) %>% summarize(mean = median(CESD_diff))
+ggplot(add_cig, aes(x = CESD_diff, fill = cigarettes, group = cigarettes)) +
+  # geom_density(alpha=.5) +
+  geom_histogram(position="identity", binwidth=1, alpha=0.6) +
+  geom_vline(data=mu, aes(xintercept=mean, color=cigarettes),
+             linetype='dashed', size=1) +
+  facet_grid(.~wave) +
+  theme_bw()
+
+# add_waves %>%
+#   select(AID, wave, cigarettes) %>%
+#   spread(wave, cigarettes) %>%
+#   rename(w1 = `1`, w2 = `2`, w3 = `3`, w4 = `4`) %>%
+#   mutate(cig = paste0(w1, w2, w3, w4)) %>%
+#   group_by(cig) %>%
+#   summarize(n = n()) %>%
+#   ggplot(aes(y = n, x = reorder(cig, n))) +
+#   geom_bar(stat='identity') +
+#   coord_flip()
+
+# add_waves %>%
+#   head(200) %>%
+#   ggplot(aes(x = wave, y = cigarettes, group = AID, col= as.factor(AID))) +
+#   geom_line(position=position_jitter(w = 0, h = 0.05)) +
+#   theme_bw() +
+#   theme(legend.position="none") +
+#   facet_wrap(~sex)
