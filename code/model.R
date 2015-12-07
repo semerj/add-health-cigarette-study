4ce966c99f3d8dd01a5e618fdbbe2c724623f758
semerdj <jsemer@gmail.com>
2015-12-06 22:53:28 -0800

    ugh

--- code/model.R
+++ code/model.R
@@ -21,8 +21,10 @@ load('../data/add_cig.RData')
 #   - CESD_diff
 
 # formulas
-# fm1 = cigarettes ~ sex + age * wave + CESD_0 + CESD_diff
-fm0 = cigarettes ~ sex + CESD_0 + CESD_diff + age
+add_cig$age_scaled = scale(add_cig$age)
+add_cig$age_min = add_cig$age - min(add_cig$age)
+add_cig$age_13 = add_cig$age - 13
+fm0 = cigarettes ~ sex + age_13 + CESD_0 + CESD_diff
 
 # cross-sectional model
 glm_m0 = glm(fm0, family='binomial', data=add_cig)
@@ -37,23 +39,12 @@ gee_exc2 = gee(fm0,    family='binomial', data=add_cig, id=AID, corstr='exchange
 
 # mixed effects model
 # help w/ convergence: https://rstudio-pubs-static.s3.amazonaws.com/33653_57fc7b8e5d484c909b615d8633c01d51.html
-fm_mix = cigarettes ~ sex + CESD_0 + CESD_diff
-mix_m0 = glmer(update(fm_mix, ~ . + (1|AID)),
+# add_cig$age_scaled = scale(add_cig$age)
+# age mean: 22.2
+# age sd:   5.15
+mix_m0 = glmer(cigarettes ~ sex + age_13 + CESD_0 + CESD_diff + (1|AID),
                family='binomial', data=add_cig,
                control=glmerControl(optimizer="bobyqa", optCtrl=list(maxfun=2e5)))
-
-mix_m1 = glmer(update(fm_mix, ~ . + (1 + CESD_diff|AID)),
-               family='binomial', data=add_cig,
-               control=glmerControl(optimizer="bobyqa", optCtrl=list(maxfun=2e5)))
-#
-# mix_m2 = glmer(update(fm_mix, ~ . + (1|wave)),
-#                family='binomial', data=add_cig)
-#
-# mix_m3 = glmer(update(fm_mix, ~ . + (1|CESD_0)),
-#                family='binomial', data=add_cig)
-#
-# mix_m4 = glmer(update(fm_mix, ~ . + (1|sex)),
-#                family='binomial', data=add_cig)
 
 summary(glm_m0)
 summary(gee_ind)
@@ -61,15 +52,34 @@ summary(gee_ind2)
 summary(gee_exc)
 summary(gee_exc2)
 summary(mix_m0)
-summary(mix_m1)
+# library(sjPlot)
+# sjt.glmer(mix_m0)
+
+AID_mean = coefficients(summary(mix_m0))[1,1]
+AID_ranef = ranef(mix_m0)$AID
+AID_ranef_sd = sqrt(VarCorr(mix_m0)$AID[[1]])
+plot(density((AID_ranef + AID_mean)[,1]))
+plot(density(rnorm(1000, AID_mean, AID_ranef_sd)))
+AID_ranef_df = data.frame(range=seq(-40,20,0.1),
+                          value=dnorm(range, mean=AID_mean, sd=AID_ranef_sd))
+ggplot(AID_ranef_df, aes(x=range, y=value)) +
+  geom_line() + theme_bw() + xlab("Log-Odds") +
+  geom_vline(x=0, col='red') +
+  annotate("text", x=-30, y=0.04,
+           label=paste0('Mean: ', round(AID_mean, 2), '\n',
+                        'SD: ', round(AID_ranef_sd, 2))) +
+  ggtitle('Distribution of Log-Odds for Subject-Specific Random Intercept')
+
+
+# summary(mix_m1)
 
 OR(glm_m0,   'glm',    se='naive')  # Logistic Regression with naive SE
 OR(glm_m0,   'glm',    se='robust', id=add_cig$AID)  # Logistic Regression with robust SE (cluster SE)
 OR(gee_ind,  'geeglm', se='robust') # GEE w/ independent correlation and robust SE
-OR(gee_ind2, 'gee',    se='robust') # GEE w/ independent correlation and robust SE
+# OR(gee_ind2, 'gee',    se='robust') # GEE w/ independent correlation and robust SE
 OR(gee_ind2, 'gee',    se='naive')  # GEE w/ independent correlation and naive SE
 OR(gee_exc,  'geeglm', se='robust') # GEE w/ exchangeable correlation and robust SE
-OR(gee_exc2, 'gee',    se='robust') # GEE w/ exchangeable correlation and robust SE
+# OR(gee_exc2, 'gee',    se='robust') # GEE w/ exchangeable correlation and robust SE
 OR(gee_exc2, 'gee',    se='naive')  # GEE w/ exchangeable correlation and naive SE
 OR(mix_m0,   'glmer',  se='naive')  # Simple Random Effects
-OR(mix_m1,   'glmer',  se='naive')  # Mixed Effects w/ random intercept and longitudinal effect
+# OR(mix_m1,   'glmer',  se='naive')  # Mixed Effects w/ random intercept and longitudinal effect
--- code/plot.R
+++ code/plot.R
@@ -1,6 +1,7 @@
 library(ggplot2)
 library(tidyr)
 library(dplyr)
+library(scales)
 
 load('../data/add_cig.RData')
 load('../data/add_waves.RData')
@@ -9,58 +10,91 @@ load('../data/add_waves.RData')
 ###################################  Plots  ####################################
 ################################################################################
 
+ggplot(add_waves, aes(x=age, y=CESD, col=sex)) +
+  geom_smooth(method='loess') +
+  coord_cartesian(ylim=c(0,10)) +
+  facet_wrap(~cigarettes) +
+  theme_bw() +
+  ylab('Depression Score (CES-D)') + xlab('Age') +
+  ggtitle('Depression Score by Sex, Cigarette Smoking Status, and Age')
+
+add_cig %>%
+  ggplot(aes(x = CESD_0, col = cigarettes)) +
+  geom_density() +
+  facet_wrap(~wave) +
+  xlab('Baseline Depression (at Wave 1)') +
+  ggtitle('Distribution of Baseline Depression by Cigarette Smoking Status and Study Wave')
+
 # Proportion of smokers by wave & sex
 add_waves %>%
   group_by(wave, sex, cigarettes) %>%
   tally() %>%
   group_by(wave, sex) %>%
-  mutate(pct=n/sum(n)) %>%
+  mutate(Percent = n/sum(n)) %>%
   filter(cigarettes == 1) %>%
-  ggplot(aes(x = wave, y = pct, col = sex, group=sex)) +
-  geom_line()
+  ggplot(aes(x = wave, y = Percent, col = sex, group=sex)) +
+  geom_line() +
+  scale_y_continuous(labels=percent) + xlab('Wave') +
+  theme_bw() +
+  ggtitle('Proportion of Cigarette Smokers by Study Wave')
+
+add_waves %>%
+  mutate(age = round(age,0)) %>%
+  group_by(age, sex, cigarettes) %>%
+  tally() %>%
+  group_by(age, sex) %>%
+  mutate(Percent = n/sum(n)) %>%
+  filter(cigarettes == 1) %>%
+  ggplot(aes(x = age, y = Percent, col = sex, group = sex)) +
+  geom_smooth(method='loess', span=1) +
+  theme_bw() +
+  scale_y_continuous(labels=percent) + xlab('Age') +
+  coord_cartesian(ylim=c(0,0.4)) +
+  ggtitle('Proportion of Cigarette Smokers by Age')
+
 
 # Each sex should have it's own intercept
 add_waves %>%
   head(200) %>%
-  ggplot(aes(x = wave, y = CESD, group = AID, col = as.factor(AID))) +
+  ggplot(aes(x = age, y = CESD, group = AID, col = as.factor(AID))) +
   geom_line(position=position_jitter(w = 0, h = 0.2)) +
   theme_bw() +
-  theme(legend.position="none") +
+  theme(legend.position='none') +
+  ylab('Depression Score') + xlab('Age') +
   facet_wrap(~sex) +
-  ggtitle('Random sample of 50 subjects')
+  ggtitle('Depression Score (CES-D) by Age\nRandom Sample of 50 subjects')
 
 # Depression went up during the second wave of the study for individuals that
 # smoked during the baseline period
-add_cig$cigarettes = as.factor(add_cig$cigarettes)
-ggplot(add_cig, aes(x = wave, y = CESD_diff, fill=cigarettes)) +
-  geom_boxplot() +
-  theme_bw()
+add_waves %>%
+  group_by(wave, cigarettes, sex) %>%
+  summarize(
+    n = n(),
+    mean = mean(CESD),
+    sd = sd(CESD),
+    se = sd/sqrt(n),
+    ci = se * qt(0.95/2 + 0.5, df=n-1)
+    ) %>%
+  ungroup() %>%
+  mutate(cigarettes = as.factor(cigarettes)) %>%
+  ggplot(aes(x = wave, y = mean, col = cigarettes, group = cigarettes)) +
+  geom_errorbar(aes(ymin=mean-ci, ymax=mean+ci), width=.1) +
+  geom_line() +
+  theme_bw() +
+  facet_grid(.~sex) +
+  ylab('Mean Depression Score (CES-D)') +
+  ggtitle('Mean Depression and 95% CI by Sex and Cigarette Smoking Status')
+
 
 # Save as above
-mu = add_cig %>% group_by(wave, cigarettes) %>% summarize(mean = median(CESD_diff))
-ggplot(add_cig, aes(x = CESD_diff, fill = cigarettes, group = cigarettes)) +
+mu = add_cig %>%
+  group_by(wave, cigarettes) %>%
+  summarize(mean = median(CESD_diff)) %>%
+  mutate(cigarettes = as.factor(cigarettes))
+ggplot(add_cig, aes(x = CESD_diff, fill = as.factor(cigarettes), group = cigarettes)) +
   # geom_density(alpha=.5) +
-  geom_histogram(position="identity", binwidth=1, alpha=0.6) +
+  geom_histogram(position='identity', binwidth=1, alpha=0.6) +
   geom_vline(data=mu, aes(xintercept=mean, color=cigarettes),
              linetype='dashed', size=1) +
   facet_grid(.~wave) +
   theme_bw()
-
-# add_waves %>%
-#   select(AID, wave, cigarettes) %>%
-#   spread(wave, cigarettes) %>%
-#   rename(w1 = `1`, w2 = `2`, w3 = `3`, w4 = `4`) %>%
-#   mutate(cig = paste0(w1, w2, w3, w4)) %>%
-#   group_by(cig) %>%
-#   summarize(n = n()) %>%
-#   ggplot(aes(y = n, x = reorder(cig, n))) +
-#   geom_bar(stat='identity') +
-#   coord_flip()
-
-# add_waves %>%
-#   head(200) %>%
-#   ggplot(aes(x = wave, y = cigarettes, group = AID, col= as.factor(AID))) +
-#   geom_line(position=position_jitter(w = 0, h = 0.05)) +
-#   theme_bw() +
-#   theme(legend.position="none") +
-#   facet_wrap(~sex)
