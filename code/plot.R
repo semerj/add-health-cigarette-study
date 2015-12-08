d7274aafcad3ccbf11640dbcd0a06b66328d15c7
semerdj <jsemer@gmail.com>
2015-12-08 12:33:35 -0800

    remove unnecessary plots

--- code/plot.R
+++ code/plot.R
@@ -10,22 +10,49 @@ load('../data/add_waves.RData')
 ###################################  Plots  ####################################
 ################################################################################
 
+# each sex should have it's own intercept
+add_waves %>%
+  filter(cigarettes == 1 & wave == 1) %>%
+  dplyr::select(AID, cigarettes) %>%
+  inner_join(add_waves, by=c('AID'='AID')) %>%
+  head(200) %>%
+  bind_rows(
+    add_waves %>%
+      filter(cigarettes == 0 & wave == 1) %>%
+      select(AID, cigarettes) %>%
+      inner_join(add_waves, by=c('AID'='AID')) %>%
+      head(200)
+  ) %>%
+  ggplot(aes(x = age, y = CESD, col = as.factor(AID))) +
+  geom_line() + geom_point() +
+  theme_bw() +
+  theme(legend.position='none') +
+  coord_cartesian(ylim=c(0,20)) +
+  facet_wrap(~cigarettes.x) +
+  ylab('Depression Score (CES-D)') + xlab('Age') +
+  ggtitle('Daily Cigarette Smoking at Wave 1 (0/1) by Depression Score and Age\nSample of 50 non-daily (0) and 50 daily smokers (1)\n') +
+  ggsave('figure1.png')
+
 ggplot(add_waves, aes(x=age, y=CESD, col=sex)) +
   geom_smooth(method='loess') +
   coord_cartesian(ylim=c(0,10)) +
   facet_wrap(~cigarettes) +
   theme_bw() +
   ylab('Depression Score (CES-D)') + xlab('Age') +
-  ggtitle('Depression Score by Sex, Cigarette Smoking Status, and Age')
+  ggtitle('LOESS Smoothing of Depression Score\nby Sex, Daily Cigarette Smoking Status (0/1), and Age\n') +
+  ggsave('figure2.png')
 
-add_cig %>%
-  ggplot(aes(x = CESD_0, col = cigarettes)) +
-  geom_density() +
+add_waves %>%
+  mutate(cigarettes = as.factor(cigarettes)) %>%
+  ggplot(aes(x = CESD, col = cigarettes)) +
+  geom_density(adjust=1.5) +
   facet_wrap(~wave) +
-  xlab('Baseline Depression (at Wave 1)') +
-  ggtitle('Distribution of Baseline Depression by Cigarette Smoking Status and Study Wave')
+  theme_bw() +
+  xlab('Depression Score (CES-D)') + ylab('') +
+  ggtitle('Distribution of Baseline Depression by Daily Cigarette Smoking Status and Study Wave\n') +
+  ggsave('figure3.png')
 
-# Proportion of smokers by wave & sex
+# proportion of smokers by wave & sex
 add_waves %>%
   group_by(wave, sex, cigarettes) %>%
   tally() %>%
@@ -36,35 +63,10 @@ add_waves %>%
   geom_line() +
   scale_y_continuous(labels=percent) + xlab('Wave') +
   theme_bw() +
-  ggtitle('Proportion of Cigarette Smokers by Study Wave')
+  ggtitle('Proportion of Cigarette Smokers by Study Wave\n') +
+  ggsave('figure4.png')
 
-add_waves %>%
-  mutate(age = round(age,0)) %>%
-  group_by(age, sex, cigarettes) %>%
-  tally() %>%
-  group_by(age, sex) %>%
-  mutate(Percent = n/sum(n)) %>%
-  filter(cigarettes == 1) %>%
-  ggplot(aes(x = age, y = Percent, col = sex, group = sex)) +
-  geom_smooth(method='loess', span=1) +
-  theme_bw() +
-  scale_y_continuous(labels=percent) + xlab('Age') +
-  coord_cartesian(ylim=c(0,0.4)) +
-  ggtitle('Proportion of Cigarette Smokers by Age')
-
-
-# Each sex should have it's own intercept
-add_waves %>%
-  head(200) %>%
-  ggplot(aes(x = age, y = CESD, group = AID, col = as.factor(AID))) +
-  geom_line(position=position_jitter(w = 0, h = 0.2)) +
-  theme_bw() +
-  theme(legend.position='none') +
-  ylab('Depression Score') + xlab('Age') +
-  facet_wrap(~sex) +
-  ggtitle('Depression Score (CES-D) by Age\nRandom Sample of 50 subjects')
-
-# Depression went up during the second wave of the study for individuals that
+# depression went up during the second wave of the study for individuals that
 # smoked during the baseline period
 add_waves %>%
   group_by(wave, cigarettes, sex) %>%
@@ -84,17 +86,3 @@ add_waves %>%
   facet_grid(.~sex) +
   ylab('Mean Depression Score (CES-D)') +
   ggtitle('Mean Depression and 95% CI by Sex and Cigarette Smoking Status')
-
-
-# Save as above
-mu = add_cig %>%
-  group_by(wave, cigarettes) %>%
-  summarize(mean = median(CESD_diff)) %>%
-  mutate(cigarettes = as.factor(cigarettes))
-ggplot(add_cig, aes(x = CESD_diff, fill = as.factor(cigarettes), group = cigarettes)) +
-  # geom_density(alpha=.5) +
-  geom_histogram(position='identity', binwidth=1, alpha=0.6) +
-  geom_vline(data=mu, aes(xintercept=mean, color=cigarettes),
-             linetype='dashed', size=1) +
-  facet_grid(.~wave) +
-  theme_bw()
